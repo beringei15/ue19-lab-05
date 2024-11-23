14f6029dcca5544a4d1c0129166298c5bd0ca8a9
beringei15 <123164423+beringei15@users.noreply.github.com>
2024-11-23 14:09:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def random_joke():
     params = {
         "type": "single",  # On récupère des blagues d'une seule ligne
         "lang": "en",      # Langue anglaise
-        "blacklistFlags": "nsfw,religious,political,racist,sexist,explicit"
+        "blacklistFlags": "nsfw,religious,political,racist,sexist,explicit"  # Pas de blague sur ces sujets là
     }
     try:
         response = requests.get(url, params=params)
@@ -17,7 +17,7 @@ def random_joke():
         
         # Vérifie si une blague a été reçue
         if joke_data.get("type") == "single":
-            print(f"Blague : {joke_data['joke']}")
+            print(f"Joke : {joke_data['joke']}")
         else:
             print("No joke found.")
     except requests.RequestException as e:
